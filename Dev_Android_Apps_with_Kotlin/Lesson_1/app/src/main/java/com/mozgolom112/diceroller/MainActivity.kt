70d8daec4bf1b24350b1032a70a74bf9babefbdd
Nikita Golovanov <cell-26@yandex.ru>
2020-07-02 08:22:35 +0300

    Finish First step: add drawable res and change TextView on ImageView

--- Dev_Android_Apps_with_Kotlin/Lesson_1/app/src/main/java/com/mozgolom112/diceroller/MainActivity.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_1/app/src/main/java/com/mozgolom112/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mozgolom112.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -15,7 +16,6 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
         rollButton.setOnClickListener {
-            //Toast.makeText(this, "button clicked",Toast.LENGTH_SHORT).show()
             rollDice()
         }
 
@@ -24,9 +24,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
 
         val randomInt: Int = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
