bd47a732c3e223cbc6978b4952f6fe299807a624
Nikita Golovanov <cell-26@yandex.ru>
2020-07-02 14:59:57 +0300

    Before add binding

--- Dev_Android_Apps_with_Kotlin/Lesson_2/AboutMe/app/src/main/java/com/mozgolom112/aboutme/MainActivity.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_2/AboutMe/app/src/main/java/com/mozgolom112/aboutme/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.mozgolom112.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.mozgolom112.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
+    //в нашем случае будет кнопка, т.е. view - кнопка view which the func was called
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE //скрываем кнопку
+        nicknameTextView.visibility = View.VISIBLE
+
+        //Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
