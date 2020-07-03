5c76650a4644065877ae859695bf354bb55fa6c8
Nikita Golovanov <cell-26@yandex.ru>
2020-07-03 08:50:37 +0300

    Add Aligned Boxes

--- Dev_Android_Apps_with_Kotlin/Lesson_2/ColorMyViews/app/src/main/java/com/mozgolom112/colormyviews/MainActivity.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_2/ColorMyViews/app/src/main/java/com/mozgolom112/colormyviews/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.mozgolom112.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners(){
+        val clickableViews: List<View> =
+            listOf(box_one_text, box_two_text, box_three_text,
+                box_four_text, box_five_text, constraint_layout)
+
+        for (item in clickableViews){
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View){
+        when (view.id){
+            R.id.box_one_text -> view.setBackgroundColor(Color.BLACK)
+            R.id.box_two_text -> view.setBackgroundColor(Color.DKGRAY)
+
+            R.id.box_three_text -> view.setBackgroundColor(Color.RED)
+            R.id.box_four_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_five_text -> view.setBackgroundColor(Color.YELLOW)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
