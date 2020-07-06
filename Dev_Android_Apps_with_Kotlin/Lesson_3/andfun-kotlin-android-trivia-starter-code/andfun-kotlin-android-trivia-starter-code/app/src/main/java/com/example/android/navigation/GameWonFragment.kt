a6d0cc0b71f74466aeb8f941074aa13759c8d6e9
Nikita Golovanov <cell-26@yandex.ru>
2020-07-06 09:08:12 +0300

    Add check for share activity

--- Dev_Android_Apps_with_Kotlin/Lesson_3/andfun-kotlin-android-trivia-starter-code/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_3/andfun-kotlin-android-trivia-starter-code/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -46,6 +46,10 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.winner_menu, menu)
+        //check if the activity resolves
+        if (null == getShareIntent().resolveActivity(requireActivity()!!.packageManager)){
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
     }
 
     private fun getShareIntent(): Intent {
