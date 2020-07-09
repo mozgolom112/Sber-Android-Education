61391cf5d52347d3171f81a4380f3a5ed1a0c8df
Nikita Golovanov <cell-26@yandex.ru>
2020-07-09 19:57:08 +0300

    Add DAO - SleepDatabaseDao

--- Dev_Android_Apps_with_Kotlin/Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,29 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SleepDatabaseDao {
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query("SELECT * FROM daily_sleep_qualities where nightId = :key")
+    fun get(key: Long): SleepNight
+
+    @Query("DELETE FROM daily_sleep_qualities")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_qualities ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_qualities ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight
+}
