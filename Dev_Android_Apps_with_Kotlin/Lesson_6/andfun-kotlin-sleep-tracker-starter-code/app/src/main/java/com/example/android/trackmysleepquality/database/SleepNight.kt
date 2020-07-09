1c9a20bb815a6d140352fd6483251f69c436f1dc
Nikita Golovanov <cell-26@yandex.ru>
2020-07-09 19:58:15 +0300

    Creating the SleepNight Entity

--- Dev_Android_Apps_with_Kotlin/Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -15,3 +15,21 @@
  */
 
 package com.example.android.trackmysleepquality.database
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ColumnInfo
+
+@Entity(tableName = "daily_sleep_qualities")
+data class SleepNight(
+        @PrimaryKey(autoGenerate = true)
+        var nightId: Long = 0L,
+
+        @ColumnInfo(name = "start_time_milli")
+        val startTimeMilli: Long = System.currentTimeMillis(),
+
+        @ColumnInfo(name = "end_time_milli")
+        var endTimeMilli: Long = startTimeMilli,
+
+        @ColumnInfo(name = "quality_rating")
+        var sleepQuality: Int = -1
+)
