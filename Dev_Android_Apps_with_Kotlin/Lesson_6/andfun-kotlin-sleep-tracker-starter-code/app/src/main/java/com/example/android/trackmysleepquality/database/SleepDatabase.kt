8256e5dfaafbdfd0569e28a5804b6d28ea064094
Nikita Golovanov <cell-26@yandex.ru>
2020-07-09 22:06:04 +0300

    Change on right type

--- Dev_Android_Apps_with_Kotlin/Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -24,7 +24,7 @@ import androidx.room.RoomDatabase
 @Database(entities = [SleepNight::class], version = 1, exportSchema = false)
 abstract class SleepDatabase: RoomDatabase() {
 
-    abstract val sleepDatabaseDao: SleepDatabase
+    abstract val sleepDatabaseDao: SleepDatabaseDao
 
     companion object{
 
