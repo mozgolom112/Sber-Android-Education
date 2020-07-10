cabfc49ce2fadbd6738fd481f9f661d54f12cf0c
Nikita Golovanov <cell-26@yandex.ru>
2020-07-10 08:55:46 +0300

    Make SleepQualityViewModel with factory

--- Dev_Android_Apps_with_Kotlin/Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -15,3 +15,48 @@
  */
 
 package com.example.android.trackmysleepquality.sleepquality
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.trackmysleepquality.database.SleepDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import kotlinx.coroutines.*
+
+class SleepQualityViewModel(
+        private val sleepNightKey: Long = 0L,
+        val database: SleepDatabaseDao): ViewModel() {
+
+    private val viewModelJob = Job()
+
+    private val uiScope = CoroutineScope(context = Dispatchers.Main + viewModelJob)
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    fun doneNavigating(){
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onSetSleepQualityClick(quality: Int){
+        uiScope.launch {
+            update(quality)
+            _navigateToSleepTracker.value = true
+        }
+    }
+
+    private suspend fun update(quality: Int) {
+        withContext(Dispatchers.IO) {
+            val tonight = database.get(sleepNightKey) ?: return@withContext
+            tonight.sleepQuality = quality
+            database.update(tonight)
+        }
+    }
+
+}
