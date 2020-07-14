4c7e2772b167fed08bb7be67c32d045c311cb614
Nikita Golovanov <cell-26@yandex.ru>
2020-07-14 18:15:20 +0300

    Add ext fun for fulfill ImageView which display Status

--- Dev_Android_Apps_with_Kotlin/Lesson_8/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/Utils.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_8/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/Utils.kt
@@ -1,11 +1,14 @@
 package com.example.android.marsrealestate
 
+import android.transition.Visibility
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.MarsApiStatus
 import com.example.android.marsrealestate.overview.adapters.PhotoGridAdapter
 
 fun ImageView.bindImage (imgUrl: String?){
@@ -20,7 +23,26 @@ fun ImageView.bindImage (imgUrl: String?){
     }
 }
 
+fun ImageView.bindStatus(status: MarsApiStatus?) {
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.loading_animation)
+        }
+
+        MarsApiStatus.ERROR -> {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.ic_connection_error)
+        }
+
+        MarsApiStatus.DONE -> {
+            visibility = View.GONE
+        }
+    }
+}
+
 fun RecyclerView.bindRecyclerView(data: List<MarsProperty>?){
     val adapter = adapter as PhotoGridAdapter
     adapter.submitList(data)
 }
+
