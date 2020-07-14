14d91a0f30665c3f23b6d411dc1e0d1408549ee6
Nikita Golovanov <cell-26@yandex.ru>
2020-07-14 20:17:30 +0300

    Add DetailFragment.kt

--- Dev_Android_Apps_with_Kotlin/Lesson_8/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_8/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -21,14 +21,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.android.marsrealestate.R
+import com.example.android.marsrealestate.bindImage
+import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
+    private val viewModel by lazy { initViewModel() }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-        val application = requireNotNull(activity).application
         val viewRoot = inflater.inflate(R.layout.fragment_detail, container, false)
+        setObservers()
         return viewRoot
     }
+
+    private fun initViewModel(): DetailViewModel {
+        val application = requireNotNull(requireActivity()).application
+        val marsProperty = DetailFragmentArgs.fromBundle(requireArguments()).selectedProperty
+        val viewModelFactory = DetailViewModelFactory(marsProperty, application)
+        val viewModel: DetailViewModel by viewModels { viewModelFactory }
+        return viewModel
+    }
+
+    private fun setObservers() {
+        viewModel.apply {
+            selectedProperty.observe(viewLifecycleOwner, Observer { marsProperty ->
+                setDetails(marsProperty)
+            })
+        }
+    }
+
+    private fun setDetails(marsProperty: MarsProperty) {
+        imgvMainPhoto.bindImage(marsProperty.imgSrcUrl)
+        txtvPropertyType.text = viewModel.displayPropertyType()
+        txtvPriceValue.text = viewModel.displayPropertyPrice()
+    }
+
 }
