91231bad68d67b63fb0f1a728337d7b2cea66343
Nikita Golovanov <cell-26@yandex.ru>
2020-07-15 20:29:31 +0300

    Add facility to save video in DB

--- Dev_Android_Apps_with_Kotlin/Lesson_9/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_9/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -17,3 +17,17 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.Video
+
+@Entity(tableName = "videos")
+class DatabaseVideo constructor(
+        @PrimaryKey
+        val url: String,
+        val updated: String,
+        val title: String,
+        val description: String,
+        val thumbnail: String
+)
+
