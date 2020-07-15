33059e52a220b5b4f9a014ebf0ca9f66d737b687
Nikita Golovanov <cell-26@yandex.ru>
2020-07-15 20:20:30 +0300

    Remove adapter and add another one

---
+++ Dev_Android_Apps_with_Kotlin/Lesson_9/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/Utils.kt
@@ -0,0 +1,44 @@
+package com.example.android.devbyteviewer
+
+import com.example.android.devbyteviewer.database.DatabaseVideo
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.NetworkVideoContainer
+
+fun List<DatabaseVideo>.asDomainModel(): List<Video> {
+    return map{
+        Video(
+                title = it.title,
+                description = it.description,
+                url = it.url,
+                updated = it.updated,
+                thumbnail = it.thumbnail)
+    }
+}
+
+/**
+ * Convert Network results to domain objects
+ */
+fun NetworkVideoContainer.asDomainModel(): List<Video> {
+    return videos.map {
+        Video(
+                title = it.title,
+                description = it.description,
+                url = it.url,
+                updated = it.updated,
+                thumbnail = it.thumbnail)
+    }
+}
+
+/**
+ * Convert Network results to database objects
+ */
+fun NetworkVideoContainer.asDatabaseModel(): Array<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo(
+                url = it.url,
+                description = it.description,
+                updated = it.updated,
+                title = it.title,
+                thumbnail = it.thumbnail)
+    }.toTypedArray()
+}
