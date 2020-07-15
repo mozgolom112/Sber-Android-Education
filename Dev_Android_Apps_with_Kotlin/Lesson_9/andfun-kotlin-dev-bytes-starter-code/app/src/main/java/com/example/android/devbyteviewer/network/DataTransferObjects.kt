9ae6090fe6ac74949bd6e333b6b5be9fa27f1b06
Nikita Golovanov <cell-26@yandex.ru>
2020-07-15 20:36:00 +0300

    Remove adapter to new file and add another one

--- Dev_Android_Apps_with_Kotlin/Lesson_9/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ Dev_Android_Apps_with_Kotlin/Lesson_9/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -50,16 +50,3 @@ data class NetworkVideo(
         val thumbnail: String,
         val closedCaptions: String?)
 
-/**
- * Convert Network results to database objects
- */
-fun NetworkVideoContainer.asDomainModel(): List<Video> {
-    return videos.map {
-        Video(
-                title = it.title,
-                description = it.description,
-                url = it.url,
-                updated = it.updated,
-                thumbnail = it.thumbnail)
-    }
-}
