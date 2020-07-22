b0e9ca32fa33699f83eb5af33e71433f12cd51e4
Nikita Golovanov <cell-26@yandex.ru>
2020-07-22 11:22:35 +0300

    Solution for ex #2

--- Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/DetailsActivity.kt
+++ Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/DetailsActivity.kt
@@ -1,10 +1,12 @@
 package com.mozgolom112.fundamentalsandroid
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mozgolom112.fundamentalsandroid.support.KEY_EXTRA_INT
 import com.mozgolom112.fundamentalsandroid.support.KEY_EXTRA_STRING
+import com.mozgolom112.fundamentalsandroid.support.URL_TO_TRAILER
 import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
@@ -12,5 +14,28 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
+        setOnClickListeners()
+        setContent()
     }
+
+    private fun setOnClickListeners() {
+        btnvWatchTrailer.setOnClickListener {
+            openUrl(URL_TO_TRAILER)
+        }
+    }
+
+    private fun openUrl(videoUrl: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(videoUrl))
+        startActivity(intent)
+    }
+
+    private fun setContent() {
+        txtvFilmTitle.text = getString(R.string.film_title)
+        txtvOverviewText.text = getString(R.string.overview_avengers_endgame)
+        txtvReleaseDate.text = getString(R.string.release_date)
+
+        imgvBackGround.setImageResource(R.drawable.endgame_background)
+        imgvSmallImage.setImageResource(R.drawable.endgame_image)
+    }
+
 }
--- Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/support/Urls.kt
+++ Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/support/Urls.kt
@@ -2,3 +2,4 @@ package com.mozgolom112.fundamentalsandroid.support
 
 //URL на гитхаб с заданиями
 const val URL_TO_GITHUB = "https://github.com/android-academy-minsk/fundamentals-2019-excercise/wiki"
+const val URL_TO_TRAILER = "https://www.youtube.com/watch?v=6ZfuNTqbHE8"
