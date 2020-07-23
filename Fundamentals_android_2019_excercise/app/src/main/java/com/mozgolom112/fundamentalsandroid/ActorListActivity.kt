74c012fb24914a232cad6f69b24b1135ae8ee9b0
Nikita Golovanov <cell-26@yandex.ru>
2020-07-23 19:35:22 +0300

    Add example from lecture with List and ListAdapter

---
+++ Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/ActorListActivity.kt
@@ -0,0 +1,21 @@
+package com.mozgolom112.fundamentalsandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mozgolom112.fundamentalsandroid.actor.Actor
+import com.mozgolom112.fundamentalsandroid.actor.ActorListAdapter
+import com.mozgolom112.fundamentalsandroid.support.DataUtil
+import kotlinx.android.synthetic.main.activity_test_actors.*
+
+class ActorListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_actors)
+
+        setAdapters()
+    }
+
+    private fun setAdapters() {
+        listvActors.adapter = ActorListAdapter(this, DataUtil.generateActors())
+    }
+}
---
+++ Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/actor/Actor.kt
@@ -0,0 +1,7 @@
+package com.mozgolom112.fundamentalsandroid.actor
+
+data class Actor(
+    val name: String,
+    val avatar: String,
+    val hasOscar: Boolean
+)
