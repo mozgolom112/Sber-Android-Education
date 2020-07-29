94ac8f3ef4ce79acddf250aa5e445025e38b5e37
Nikita Golovanov <cell-26@yandex.ru>
2020-07-29 07:38:34 +0300

    Testing view pager

--- Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/MovieActivity.kt
+++ Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/MovieActivity.kt
@@ -3,11 +3,24 @@ package com.mozgolom112.fundamentalsandroid
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.mozgolom112.fundamentalsandroid.adapters.DetailsPagerAdapter
+import com.mozgolom112.fundamentalsandroid.support.DataUtil
+import kotlinx.android.synthetic.main.fragment_gallery_details.*
 
 class MovieActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_movie)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_gallery_details)
 
+        val pagerAdapter = DetailsPagerAdapter(supportFragmentManager, DataUtil.generateMovies())
+        vpPager.adapter = pagerAdapter
+    }
+
+    override fun onBackPressed() {
+        val currentItem = vpPager.currentItem
+        val isBegin = currentItem == 0
+
+        if (isBegin) super.onBackPressed() else vpPager.currentItem = currentItem - 1
     }
 }
--- Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/adapters/DetailsPagerAdapter.kt
+++ Fundamentals_android_2019_excercise/app/src/main/java/com/mozgolom112/fundamentalsandroid/adapters/DetailsPagerAdapter.kt
@@ -1,19 +1,17 @@
 package com.mozgolom112.fundamentalsandroid.adapters
 
-import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.mozgolom112.fundamentalsandroid.MovieDetailsFragment
 import com.mozgolom112.fundamentalsandroid.models.Movie
 
-class DetailsFragmentAdapter(
+class DetailsPagerAdapter(
     fragmentManager: FragmentManager,
     private val listOfMovies: List<Movie>
     //TODO('Когда нибудь заменить на ViewPager2')
     ) : FragmentStatePagerAdapter(fragmentManager){
-    override fun getItem(position: Int): Fragment {
-        TODO("Not yet implemented")
-    }
+    override fun getItem(position: Int): Fragment = MovieDetailsFragment()
 
     override fun getCount(): Int = listOfMovies.size
 }
